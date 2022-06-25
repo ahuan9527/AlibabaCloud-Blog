0e286d890c05d149ae4d46c49e5cc0e71be09710
Jhuan <a1875884755@gmail.com>
2022-06-25 18:33:34 +0800

    增加启动配置文件

--- sentinel-blog/src/main/java/org/foon/sentinel/config/Env.java
+++ sentinel-blog/src/main/java/org/foon/sentinel/config/Env.java
@@ -1,6 +1,7 @@
 package org.foon.sentinel.config;
 
 import com.alibaba.csp.sentinel.node.NodeBuilder;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @Description <What does this class do>
@@ -10,6 +11,7 @@ import com.alibaba.csp.sentinel.node.NodeBuilder;
  * @Email a1875884755@gmail.com
  * @Version 1.0
  */
+@Configuration
 public class Env{
 
 }
